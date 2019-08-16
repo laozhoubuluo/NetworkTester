6e02ede447d43ab5fb72e4dd0470296ede705493
老周部落 <31075951+laozhoubuluo@users.noreply.github.com>
2019-08-16 12:40:31 +0800

    同步上游变更

--- pingTest.sh
+++ pingTest.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 pingTo=www.baidu.com
-#pingTo=baidu.com
+port=80
+dns=114.114.114.114
 #pingTo=10.99.200.210
-ping $pingTo -c 3 -W 1
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+#ping $pingTo -c 3 -W 1
+$SHELL_FOLDER/tcping $pingTo $port -D $dns -c 3
